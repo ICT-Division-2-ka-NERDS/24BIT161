afa785a37f108bc74483d7f3ab9df5ae2000950f
Chawal20 <makhijayesha@gmail.com>
2025-04-21 21:46:59 +0530

    Delete 24.py

--- 24.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter one number= "))
-b=int(input("Enter another number= "))
-print("Numbers before swapping a=",a," b=",b)
-c=a
-a=b
-b=c
-print("Numbers after swapping a=",a," b=",b)
