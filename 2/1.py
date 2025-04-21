89ba40bfdbbfc70174116cfa23c32084f1c6bd95
Chawal20 <makhijayesha@gmail.com>
2025-04-21 22:08:56 +0530

    Create 1.py

---
+++ 2/1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter first value= "))
+b=int(input("Enter second value= "))
+if a>b:
+  print(a, " is the largest value.")
+  print(b, " is the smallest value.")
+else:
+  print(b, " is the largest value.")
+  print(a, " is the smallest value.")
